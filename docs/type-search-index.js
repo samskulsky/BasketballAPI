f9c2358acd0ceaa89642c3c4674e29567ebdb23d
Sam Skulsky <69123082+Bonunu202@users.noreply.github.com>
2021-06-13 23:10:32 -0700

    javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"GameStatus"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"PlayerStats"},{"p":"<Unnamed>","l":"Team"}];updateSearchResults();
